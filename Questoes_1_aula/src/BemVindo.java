99fa3ab940c37c93ea4a0555c1789858c0ff1e3e
nicolashsousa <nicolashsc06@gmail.com>
2026-03-11 19:19:37 -0300

    rejuste

---
+++ Questoes_1_aula/src/BemVindo.java
@@ -0,0 +1,7 @@
+//Classe Bem vindo
+public class BemVindo {
+    //Metodo Main
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    } //Fim do Método
+}//Fim da classe
