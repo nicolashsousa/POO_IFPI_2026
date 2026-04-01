f1033188d2fb6ba96456cc12f2dd45acab345315
nicolashsousa <nicolashsc06@gmail.com>
2026-03-31 23:52:09 -0300

    rejuste

--- src/aula3/UsaConta.java
+++ src/aula3/UsaConta.java
@@ -3,11 +3,10 @@ package aula3;
 public class UsaConta {
     public static void main(String[] args) {
         Conta minhaConta = new Conta();
-
+        
         minhaConta.setTitular("Fulano");
         minhaConta.depositar(1000);
         minhaConta.sacar(370);
-        minhaConta.equals(minhaConta);
 
         System.out.println("Conta: "+minhaConta.getTitular());
         System.out.println("Saldo: "+minhaConta.getSaldo()+" R$");
