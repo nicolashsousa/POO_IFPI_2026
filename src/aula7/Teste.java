5851159748678fd286958f18ac09f667a0735dbf
nicolashsousa <nicolashsc06@gmail.com>
2026-05-13 19:45:32 -0300

    Swing

---
+++ src/aula7/Teste.java
@@ -0,0 +1,42 @@
+package aula7;
+
+import java.awt.FlowLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+public class Teste {
+    public static void main(String[] args) {
+        JFrame meuFrame = new JFrame();
+        
+        meuFrame.setTitle("Calculadora de média");
+        meuFrame.setSize(200,200);
+        meuFrame.setLayout(null);
+        meuFrame.setLayout(new FlowLayout());
+
+        JLabel matricuLabel = new JLabel("Matricula: ");
+        JTextField matriculaField = new JTextField(15);
+
+        JLabel notaLabel = new JLabel("Media: ");
+        JTextField notaField = new JTextField(15);
+
+        JButton verificarButton = new JButton();
+        verificarButton.setText("Cadastrar");
+
+        JButton cancelarButton = new JButton();
+        cancelarButton.setText("Cancelar");
+
+        meuFrame.add(matricuLabel);
+        meuFrame.add(matriculaField);
+
+        meuFrame.add(notaLabel);
+        meuFrame.add(notaField);
+
+        meuFrame.add(verificarButton);
+        meuFrame.add(cancelarButton);
+
+        meuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        meuFrame.setVisible(true);
+    }
+}
